9ad7b5f327ad08da0d2e53a24749f5683ca71fd0
jingsam <jing-sam@qq.com>
2025-04-08 17:33:27 +0800

    修复supabase-storage错误

---
+++ docker/volumes/db/0035-add-insert-trigger-prefixes.sql
@@ -0,0 +1,9 @@
+
+-- This trigger is used to create the hierarchy of prefixes
+-- When writing directly in the prefixes table
+DROP TRIGGER IF EXISTS "prefixes_create_hierarchy" ON "storage"."prefixes";
+CREATE TRIGGER "prefixes_create_hierarchy"
+    BEFORE INSERT ON "storage"."prefixes"
+    FOR EACH ROW
+    WHEN (pg_trigger_depth() < 1)
+EXECUTE FUNCTION "storage"."prefixes_insert_trigger"();
