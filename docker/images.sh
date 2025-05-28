5ae70180c63726aa1f7ac27953f2472a556cbb7e
jingsam <jing-sam@qq.com>
2025-05-28 19:34:33 +0800

    调整镜像打包脚本

--- docker/images.sh
+++ docker/images.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-IMAGES=($(awk '/^[[:blank:]]+image:/ {sub(/^[[:blank:]]+image:[[:blank:]]*"?/,""); gsub(/"|'"'"'/,""); print}' docker-compose.yml | sort -u))
+set -euo pipefail
 
 PLATFORM=linux/amd64
 
+# 解析镜像列表
+IMAGES=($(awk '/^[[:blank:]]+image:/ {sub(/^[[:blank:]]+image:[[:blank:]]*"?/,""); gsub(/"|'"'"'/,""); print}' docker-compose.yml | sort -u))
+
+# 删除旧镜像
 docker rmi "${IMAGES[@]}" 2>/dev/null
 
+# 拉取镜像
 for image in "${IMAGES[@]}"; do
   docker pull --platform $PLATFORM $image
 done
 
-# Extract arch from $PLATFORM
-docker save -o images-${PLATFORM##*/}.tar "${IMAGES[@]}"
+# 导出为镜像包
+docker save "${IMAGES[@]}" | gzip > images-${PLATFORM##*/}.tar.gz
